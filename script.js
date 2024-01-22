1569f652699cf4857e5f9ca3cea92adf4fc4c192
okcabroad <okcabroad@gmail.com>
2024-01-22 13:54:53 +0900

    adding the hover capability to the grid

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const gridContainer = document.getElementById("gridContainer");
 
+
+//creating a 16 by 16 grid
 for(i = 0; i < 16; i++) {
     for(j = 0; j < 16; j++){
-    const cell = document.createElement("div");
+    const cell = document.createElement('div');
     cell.classList.add('cell');
     gridContainer.appendChild(cell);
-
     }
 }
+
+const cells = document.getElementsByClassName("cell")
+for (let i = 0; i < cells.length; i++){
+    cells[i].addEventListener("mouseover", color, false);
+}
+
+function color(event) {
+    event.target.style.backgroundColor = "blue";
+    console.log(event.target) //seeing if color function is activating
+}
+
