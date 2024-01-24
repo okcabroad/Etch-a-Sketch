07e21324fa9984cfa84f3555c38a71511d07d987
okcabroad <okcabroad@gmail.com>
2024-01-25 00:50:20 +0900

    making the grid continer fixed and cells fit to size inside the container. adding a random rgb value to the color function

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const gridBtn = document.getElementById("gridBtn");
 
 //Initial grid setup
 let gridSize = 16;
-gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+gridContainer.style.width = "400px"; // Set a fixed width for the grid container
+gridContainer.style.height = "400px"; // Set a fixed height for the grid container
+gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 createGrid();
 addEventListeners();
 
 
-//button event listener
+//Creating the button prompt for Grid Dimensions
 gridBtn.addEventListener('click', function() {
     const userInput = prompt("Enter a number between 1 - 100");
     console.log(userInput)
     addGridSize(userInput)
 });
 
+//Validating the user input and creating the new grid dimensions
 function addGridSize(userInput) {
 
     if(userInput === null || userInput < 1 || userInput > 100) {
@@ -31,7 +34,7 @@ function addGridSize(userInput) {
 }
 
 
-//creating a 16 by 16 grid
+//Creating the Grid 
 function createGrid() {
 for(let i = 0; i < gridSize; i++) {
     for(let j = 0; j < gridSize; j++){
@@ -42,6 +45,15 @@ for(let i = 0; i < gridSize; i++) {
     }
 }
 
+//reset the grid
+function resetGrid() {
+    let gridSize = 16;
+    createGrid();
+    addEventListeners();
+
+}
+
+//addEventListenter to color the cells of the grid 
 function addEventListeners(){
 const cells = document.getElementsByClassName("cell")
 for (let i = 0; i < cells.length; i++){
@@ -49,10 +61,13 @@ for (let i = 0; i < cells.length; i++){
     }
 }
 
-
+//function to color the grid 
 function color(event) {
-    event.target.style.backgroundColor = "red";
-    console.log(event.target) //seeing if color function is activating
+    const randomRed = Math.floor(Math.random() * 256);
+    const randomGreen = Math.floor(Math.random() * 256);
+    const randomBlue = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+
 }
 
 
